517772e54577c063aea035a15320bd26a0567fb1
Jenna Toff <jennatoff@comcast.net>
2020-05-23 12:00:10 -0700

    icons now buttons, syntax fixed

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 // Let's find the form in the DOM
-let formElement = document.querySelector(".popout__form") // Use the querySelector() method
-
-// Next is the form submit handler, though
-// it won't submit anywhere just yet
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-                                                // Having done so, we can define our own way of submitting the form.
-                                                // We'll explain it in more detail later.
-
-    // Let's find the form fields in the DOM
-    let nameInput = formElement.querySelector("#name")// Use querySelector()
-    let jobInput = formElement.querySelector("#about")// Use querySelector()
-
-
-    // Get the values of each field from the corresponding value property
-
-    // Select elements where the field values will be entered
-
-    document.querySelector(".profile__name").textContent = nameInput.value;
-    
-    document.querySelector(".profile__profession").textContent = jobInput.value;// Insert new values using the textContent property of the querySelector() method
-}
-
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', formSubmitHandler);
-
+const formElement = document.querySelector(".popout__form");
+const nameInput = formElement.querySelector(".popout__form-input_type_name");
+const nameOutput = document.querySelector(".profile__name");
+const jobInput = formElement.querySelector(".popout__form-input_type_job");
+const jobOutput = document.querySelector(".profile__profession");
 const editBtn = document.querySelector(".profile__edit-button");
 const closeBtn = document.querySelector(".popout__close-button");
 
-function visible() {   
-    document.querySelector(".popout__container").classList.add("popout__container_active");
-    return;
-}
-function hidden() {
-    document.querySelector(".popout__container").classList.remove("popout__container_active");
-    return;
+
+function toggle() {   
+    document.querySelector(".popout__container").classList.toggle("popout__container_active");
 }
 
-editBtn.addEventListener("click", visible);
-closeBtn.addEventListener("click", hidden);
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    nameOutput.textContent = nameInput.value;
+    
+    jobOutput.textContent = jobInput.value;
+    
+    toggle()
+}
+
+
+
+formElement.addEventListener('submit', formSubmitHandler);
+editBtn.addEventListener("click", toggle);
+closeBtn.addEventListener("click", toggle);
