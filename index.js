6e70c0834bd41b6cfc7d6a8299383e9c59802da3
Jenna Toff <jennatoff@comcast.net>
2020-06-03 16:09:03 -0700

    add files

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 // Let's find the form in the DOM
-const formElement = document.querySelector(".popout__form");
-const nameInput = formElement.querySelector(".popout__form-input_type_name");
+const profileFormElement = document.querySelector(".popout_profile-edit__form");
+const galleryFormElement = document.querySelector(".popout_gallery-add__form");
+const nameInput = profileFormElement.querySelector(".popout__form-input_type_name");
 const nameOutput = document.querySelector(".profile__name");
-const jobInput = formElement.querySelector(".popout__form-input_type_job");
+const jobInput = profileFormElement.querySelector(".popout__form-input_type_job");
 const jobOutput = document.querySelector(".profile__profession");
 const editBtn = document.querySelector(".profile__edit-button");
-const closeBtn = document.querySelector(".popout__close-button");
-const popout = document.querySelector(".popout__container");
+const addButton = document.querySelector(".profile__add-button");
+const profileCloseBtn = document.querySelector(".popout_profile-edit__close-button");
+const galleryCloseBtn = document.querySelector(".popout_gallery-add__close-button");
+const profilePopout = document.querySelector(".popout_profile-edit__container");
+const galleryPopout = document.querySelector(".popout_gallery-add__container");
 
-function toggle() {   
-    popout.classList.toggle("popout__container_active");
+function profileToggle() {   
+    profilePopout.classList.toggle("popout__container_active");
+}
+function galleryToggle(){
+    galleryPopout.classList.toggle("popout__container_active");
 }
 
-function formSubmitHandler (evt) {
+function profileFormSubmitHandler (evt) {
     evt.preventDefault();
 
     nameOutput.textContent = nameInput.value;
     
     jobOutput.textContent = jobInput.value;
     
-    toggle()
+    profileToggle()
+}
+function galleryFormSubmitHandler (evt) {
+    evt.preventDefault();
+    
+    // input html for a gallery container to be appended at the end
+}
+function galleryDelete (){
+    //delete button removes html for specific element
 }
 
 
-
-formElement.addEventListener('submit', formSubmitHandler);
-editBtn.addEventListener("click", toggle);
-closeBtn.addEventListener("click", toggle);
+profileFormElement.addEventListener('submit', profileFormSubmitHandler);
+editBtn.addEventListener("click", profileToggle);
+profileCloseBtn.addEventListener("click", profileToggle);
+addButton.addEventListener("click", galleryToggle);
+galleryCloseBtn.addEventListener("click", galleryToggle);
